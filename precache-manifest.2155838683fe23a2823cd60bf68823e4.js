4c264a938f1db683f79207a8a93b2daea8c34028
kmladenov <ash.kinkoid@gmail.com>
2020-03-25 13:23:10 +0200

    Updates

---
+++ precache-manifest.2155838683fe23a2823cd60bf68823e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b43b8be964a860fafca65bfe1e303178",
+    "url": "/dhl-gallery-reactjs/index.html"
+  },
+  {
+    "revision": "2aafda60b8db3420cbf9",
+    "url": "/dhl-gallery-reactjs/static/css/main.32337a2c.chunk.css"
+  },
+  {
+    "revision": "92a534ae1c27dac30b95",
+    "url": "/dhl-gallery-reactjs/static/js/2.553938f3.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/dhl-gallery-reactjs/static/js/2.553938f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aafda60b8db3420cbf9",
+    "url": "/dhl-gallery-reactjs/static/js/main.4d7e3368.chunk.js"
+  },
+  {
+    "revision": "86ffb541ca8bdcc728c9",
+    "url": "/dhl-gallery-reactjs/static/js/runtime-main.45d6cbae.js"
+  }
+]);
